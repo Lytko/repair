dbb8fc91d4b87838e9b3ea1e8c49b06757aab64f
Alex <sashalytko@mail.ru>
2018-12-03 20:01:04 +0100

    Uodate

--- smart.php
+++ smart.php
@@ -3,6 +3,12 @@
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 
+$name = $_POST['user_name1'];
+$phone = $_POST['user_phone1'];
+
+$name = $_POST['user_name2'];
+$phone = $_POST['user_phone2'];
+
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
@@ -12,12 +18,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'agrids12345@gmail.com';                 // Наш логин
+$mail->Username = 'alexlytko12345@mail.ru';                 // Наш логин
 $mail->Password = '032ujfw9';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('agrids12345@gmail.com', 'Alex');   // От кого письмо 
+$mail->setFrom('alexlytko12345@mail.ru', 'Alex');   // От кого письмо 
 $mail->addAddress('jijanisa@idx4.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
